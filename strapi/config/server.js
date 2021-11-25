21bc4b18c88591919e363e4831f197ccc539263f
Sdqumar <sdqumar09@gmail.com>
2021-11-25 23:58:59 +0000

    yarn starrt

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1337 || process.env.port),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a3cfe5da026d44df7a568cb6b3012f86'),
